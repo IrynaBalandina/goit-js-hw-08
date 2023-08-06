7805413f9bd3c15853bca8bbcbab57766634fab7
Iryna <mefodievna2908@gmail.com>
2023-08-06 13:47:03 +0300

    HW part 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`;
+  })
+  .join('');
+
+galleryList.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+ 
+  captions: true,
+  captionsData: 'alt',
+  captionSelector: 'img',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
